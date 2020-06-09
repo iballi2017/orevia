ccea34d139cd6aa625aad2ddce40a59203e943ec
iballi2017 <iballi2017@gmail.com>
2020-06-10 00:03:34 +0100

    09/06/2020

---
+++ components/newsletter_form_field.php
@@ -0,0 +1,16 @@
+<form action="#">
+    <p class="mt-5">
+        Subscribe to our newsletter to get update our latest offers and
+        product
+    </p>
+
+    <div class="newsletter-form-grp">
+        <input type="email" name="email" id="newsletter_email_input" placeholder="Enter Your Email Address"
+            aria-label="Enter Your Email Address" class="pl-3" />
+        <button type="submit">
+        </button>
+    </div>
+    <div id="newsletter_email_error_mgs">
+        <small class="text-danger">Please enter a valid email address</small>
+    </div>
+</form>
--- index.php
+++ index.php
@@ -526,23 +526,9 @@
             <div class="row">
                 <div class="col-12 col-md-4 border-right pr-5 my-3">
                     <img src="./assets/images/orevia-Logo.png" alt="" />
-                    <form action="#">
-                        <p class="mt-5">
-                            Subscribe to our newsletter to get update our latest offers and
-                            product
-                        </p>
 
-                        <div class="newsletter-form-grp">
-                            <input type="email" name="email" id="newsletter_email_input"
-                                placeholder="Enter Your Email Address" aria-label="Enter Your Email Address"
-                                class="pl-3" />
-                            <button type="submit">
-                            </button>
-                        </div>
-                        <div id="newsletter_email_error_mgs">
-                            <small class="text-danger">Please enter a valid email address</small>
-                        </div>
-                    </form>
+                    <!-- newsletter form field -->
+                    <?php include_once './components/newsletter_form_field.php'; ?>
                 </div>
 
                 <div class="col-12 col-md-4 my-3">
